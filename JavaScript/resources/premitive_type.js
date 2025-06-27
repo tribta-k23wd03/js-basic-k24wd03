388e1d1e8f61f3e268a1b35aa9147e7e46e0762f
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-06-27 14:46:32 +0700

    premitive_type

---
+++ JavaScript/resources/premitive_type.js
@@ -0,0 +1,75 @@
+/**
+ * ==============================
+ * LESSON: Khai báo Biến
+ * ==============================
+ */
+
+//
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+/* */
+// identifier
+
+// number data type
+let x = 1;
+let z = 0.1;
+
+// string
+let y = "a";
+
+// boolean
+let b = true;
+
+// undifined - không xác định
+let n;
+
+// undifined + number = ???
+console.log(n + x); // ??? Not a Number NaN
+
+// null
+let abc = null;
+let f = undefined;
+
+// Name of Variable
+let myName = "Donald Trump";
+console.log(myName);
+
+// let my-name
+let my_name;
+// camel case
+let doNamTrum = "";
+
+//let 1one;
+
+let productName = "laptop";
+let zxy = "laptop";
+
+// let product name;
+
+let p,
+  o = 1;
+
+// let t = "A", g = "Z";
+
+let t = "A";
+let g = "Z";
+
+// 
+let u = 1;
+u = 0.1111111111111111111;
+
+let st = "abc";
+st = 'z';
+st = 1; // 1 
+
+// constant
+const ki = 1;
+// ki = "string";
+ki = 2;
+
+// Premitive type.
