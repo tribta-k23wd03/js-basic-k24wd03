2b2753021fdd75067a7d6c5968a66520e52e9272
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-21 15:02:00 +0700

    update resources

---
+++ DOM-2/resources/DOM/index.js
@@ -0,0 +1,75 @@
+/**
+ * ==============================
+ * LESSON: Event
+ * ==============================
+ */
+
+const btn = document.querySelector("button");
+
+// 0.1 ~ 0
+// 0.5 ~ 0
+// 0.9 ~ 0
+function random(number) {
+  return Math.floor(Math.random() * (number + 1)); // 0 -> 1 (0.1,0.01,0.001,...)
+  // 0 ---> 1 * 255
+  // 0.9 * 255 ~
+}
+
+// khởi tạo event:
+btn.addEventListener("click", () => {
+  const randomColor = `rgb(${random(255)},${random(255)},${random(255)})`;
+  document.body.style.backgroundColor = randomColor;
+  // 0 --> 255
+  //   document.body.style.backgroundColor = rgb(
+  //     random(255), // return random number,
+  //     random(255), // return random number,
+  //     random(255) // return random number,
+  //   );
+});
+
+/**
+ * ==============================
+ * LESSON: Event: Read KeyStroke
+ * ==============================
+ */
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `You pressed "${event.key}".`;
+});
+
+/**
+ * ==============================
+ * LESSON: Event: Form Validation
+ * ==============================
+ */
+
+const form = document.querySelector("form");
+const fName = document.getElementById("fName");
+const lName = document.getElementById("lName");
+
+const para = document.querySelector("p");
+
+form.addEventListener("submit", (e) => {
+  if (fName.value === "" || lName.value === "") {
+    e.preventDefault;
+    para.textContent = "You need to enter FirstName or LastName!!!";
+  }
+});
+
+/**
+ * ==============================
+ * LESSON: Event: Change Color Text.
+ * ==============================
+ */
+function changeText() {
+  const p = document.getElementById("pid");
+
+  p.style.color = "tomato";
+  p.style.fontSize = "18pt";
+}
+
+document.getElementById("btn-change").addEventListener("click", () => {
+  changeText();
+});
