21ee9e105b7aee450b4e7650da3911a28c4a7cab
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-21 16:25:48 +0700

    update

--- DOM-2/index.js
+++ DOM-2/index.js
@@ -1,12 +1,33 @@
-const btn = document.querySelector("button");
-const box = document.querySelector("div");
-const video = document.querySelector("video");
+/**
+ * LESSON: Parent Event Vs Child Event
+ */
+// const btn = document.querySelector("button");
+// const box = document.querySelector("div");
+// const video = document.querySelector("video");
 
-btn.addEventListener("click", () => box.classList.remove("hidden"));
+// btn.addEventListener("click", () => box.classList.remove("hidden"));
 
-video.addEventListener("click", (event) => {
-  event.stopPropagation();
-  video.play();
+// video.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   video.play();
+// });
+
+// box.addEventListener("click", () => box.classList.add("hidden"));
+
+/**
+ * LESSON: Phân quyền Event - Event Delegation
+ */
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function bgChange() {
+  const randColor = `rgb(${random(255)},${random(255)},${random(255)})`;
+  return randColor;
+}
+
+const container = document.querySelector("#container");
+
+container.addEventListener("click", (event) => {
+  event.target.style.backgroundColor = bgChange();
 });
-
-box.addEventListener("click", () => box.classList.add("hidden"));
